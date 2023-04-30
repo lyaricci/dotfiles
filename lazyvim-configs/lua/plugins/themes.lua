0e5082070a64f034fa27bc62fa83d1fe5ee4d765
Nathálya Ricci <102884724+nathalya-ricci@users.noreply.github.com>
2023-04-30 18:40:41 -0300

    add moonfly theme

--- lazyvim-configs/lua/plugins/themes.lua
+++ lazyvim-configs/lua/plugins/themes.lua
@@ -3,10 +3,12 @@ return {
 
 	{ "tomasiser/vim-code-dark" },
 
+	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "codedark",
+			colorscheme = "moonfly",
 		},
 	},
 }
