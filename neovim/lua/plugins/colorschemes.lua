24a41b4c8e0dd9c551e70f42d8ce04c00a34efc8
Nathálya Ricci <102884724+nathalya-ricci@users.noreply.github.com>
2023-05-09 19:34:10 -0300

    add base16 themes for neovim

--- neovim/lua/plugins/colorschemes.lua
+++ neovim/lua/plugins/colorschemes.lua
@@ -1,6 +1,6 @@
-require("notify").setup({
-  background_colour = "#000",
-})
+-- require("notify").setup({
+-- 	background_colour = "#000",
+-- })
 
 -- THEMES
 require("plugins.themes.catppuccin")
@@ -10,10 +10,11 @@ require("plugins.themes.moonfly")
 
 return {
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	{ "RRethy/nvim-base16" },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "base16-material-darker",
+		},
+	},
 }
