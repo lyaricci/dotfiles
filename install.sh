0505829ad475e0bf3603781fbcbd312268697285
Nathálya Ricci <102884724+lyaricci@users.noreply.github.com>
2023-05-11 16:57:09 -0300

    add lazygit installation

--- install.sh
+++ install.sh
@@ -39,3 +39,10 @@ echo "Installing snap"
 # Install fonts
 ln -s ~/dotfiles/fonts ~/.local/share/fonts
 echo "Installing fonts"
+
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+echo "Installing lazygit"
