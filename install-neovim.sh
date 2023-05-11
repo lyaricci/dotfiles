83a30f1c5bc2108031a97794fd4438e4273f3c7a
Nathálya Ricci <nathalyarici@gmail.com>
2023-05-11 16:39:47 -0300

    fix neovim install

--- install-neovim.sh
+++ install-neovim.sh
@@ -4,6 +4,12 @@
 sudo apt install neovim
 echo "Installing Neovim"
 
+# Install LazyVim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+rm -rf ~/.config/nvim/lua
+echo "Installing LazyVim"
+
 # Symlink lazynvim configs
 ln -s ~/dotfiles/neovim/lua ~/.config/nvim/
 echo "Linking nvim files"
