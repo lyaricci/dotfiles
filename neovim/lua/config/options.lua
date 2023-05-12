671bb91c0ee7ad689810e06e6a239909e7bcb634
Nathálya Ricci <102884724+lyaricci@users.noreply.github.com>
2023-05-12 00:02:33 -0300

    set xclip as clipboard

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -3,19 +3,19 @@
 -- Add any additional options here
 
 local options = {
-  backup = false, -- creates a backup file
-  clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-  cmdheight = 1, -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  conceallevel = 0, -- so that `` is visible in markdown files
-  hlsearch = true, -- highlight all matches on previous search pattern
-  ignorecase = true, -- ignore case in search patterns
-  smartcase = true, -- smart case
-  smartindent = true, -- make indenting smarter again
-  swapfile = false, -- creates a swapfile
-  expandtab = true, -- convert tabs to spaces
-  shiftwidth = 2, -- the number of spaces inserted for each indentation
-  tabstop = 2, -- insert 2 spaces for a tab
-  numberwidth = 2, -- set number column width to 2 {default 4}
-  wrap = true, -- display lines as one long line
+	backup = false, -- creates a backup file
+	clipboard = "xclip", -- allows neovim to access the system clipboard
+	cmdheight = 1, -- more space in the neovim command line for displaying messages
+	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+	conceallevel = 0, -- so that `` is visible in markdown files
+	hlsearch = true, -- highlight all matches on previous search pattern
+	ignorecase = true, -- ignore case in search patterns
+	smartcase = true, -- smart case
+	smartindent = true, -- make indenting smarter again
+	swapfile = false, -- creates a swapfile
+	expandtab = true, -- convert tabs to spaces
+	shiftwidth = 2, -- the number of spaces inserted for each indentation
+	tabstop = 2, -- insert 2 spaces for a tab
+	numberwidth = 2, -- set number column width to 2 {default 4}
+	wrap = true, -- display lines as one long line
 }
